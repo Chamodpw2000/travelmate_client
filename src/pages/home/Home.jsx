dde3c6da8edd3402aaeb5d15e10dc39dd7ed67a4
chamodpw2000 <mailtochamodw@gmail.com>
2025-12-17 23:28:45 +0530

    feat: hide villas from landing page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
       <AiTripPlanning />
       <HotelsInLandingPage />
       <RestaurantsInLandingPage />
-      <VillasInLandingPage />
+      {/* <VillasInLandingPage /> */}
       <TransportServisersInLandingPage />
       <GuideList />
       {/* <Activities /> */}
